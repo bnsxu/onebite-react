3f5d6e6e183c98397e819ce6da63f639e1e95410
sxu <bnsxu@naver.com>
2024-08-25 00:19:30 +0900

    반복문으로 배열과 객체 순회하기

---
+++ section02/chapter06.js
@@ -0,0 +1,57 @@
+//1. 배열 순회
+let arr = [1, 2, 3];
+
+//1-1. 배열 인덱스 (인덱스 저장 O)
+//length: 배열의 길이를 저장
+for(let i=0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+let arr2 = [4, 5, 6, 7, 8];
+for(let i = 0; i < arr2.length; i++){
+    console.log(arr2[i]);
+}
+
+//1-2. for of 반복문: 오직 배열을 순회하기 위해서만 사용(인덱스 저장 X)
+for(let item of arr){
+    console.log(item);
+}
+
+//2. 객체 순회
+let person = {
+    name: "이정환",
+    age: 27,
+    hobby: "테니스",
+};
+
+//2-1. Object.key 사용: 객체에서 key 값들만 뽑아서 새로운 배열로 반환
+let keys = Object.keys(person);
+console.log(keys);
+
+for(let i = 0; i < keys.length; i++){
+    console.log(keys[i]);
+}
+
+for(let key of keys){
+    //key : value 출력하기
+    const value = person[key];
+    console.log(key, value);
+}
+
+//2-2. Object.values: 객체에서 value 값들만 뽑아서 새로운 배열로 반환
+let values = Object.values(person);
+console.log(values);
+
+for(let value of values){
+    console.log(value);
+}
+
+//2-3. for in: 객체에만 사용 가능
+for(let key in person){
+    console.log(key);
+}
+
+for(let key in person){
+    const value = person[key];
+    console.log(key, value);
+}
