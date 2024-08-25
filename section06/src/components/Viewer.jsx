c3afe0396d8756f35bbfdff45e34aaab6f1b7fac
sxu <bnsxu@naver.com>
2024-08-25 19:11:47 +0900

    기능 구현하기

--- section06/src/components/Viewer.jsx
+++ section06/src/components/Viewer.jsx
@@ -1,8 +1,10 @@
-const Viewer = () => {
+//구조 분해 할당으로 {count} 받아오기
+const Viewer = ({count}) => {
+    
     return (
     <div>
         <div>현재 카운트: </div>
-        <h1>0</h1>
+        <h1>{count}</h1>
     </div>  
     );
 };
