e66ded58e4c519eeeffe073089a158b4d43bc326
sxu <bnsxu@naver.com>
2024-08-25 15:41:59 +0900

    이벤트 처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,9 +1,19 @@
 const Button = ({text, color, children}) => {
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    };
+    
     //{text} App.jsx에서 설정한 text를 넣어준다
-    return <button style={{color: color}}>
+    return (
+    <button 
+    onClick={onClickButton}
+    //onMouseEnter={onClickButton}
+    style={{color: color}}>
         {text}-{color.toUpperCase()}
         {children}
         </button>
+    );
 };
 
 Button.defaultProps = {
