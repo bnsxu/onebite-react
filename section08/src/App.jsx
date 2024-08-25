86389f8e6cb6d04cc9c79ffcc9c00f932a4c88b5
sxu <bnsxu@naver.com>
2024-08-25 22:26:03 +0900

    Create - 투두 추가하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,14 +1,43 @@
+import './App.css'
+import { useState, useRef } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
 import TodoItem from './components/TodoItem';
-import './App.css'
 
-function App() {
+const mokData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {  
+  const [todos, setTodos] = useState(mokData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    }
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
